eeaf06ea3c66de7c2acb3992b5d8d3932f20f9d2
David Everett <davideverett1989@gmail.com>
2020-06-15 21:48:19 -0500

    All page components are in place.

---
+++ src/components/pages/EditToast/EditToast.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './EditToast.scss';
+
+class EditToast extends React.Component {
+  render() {
+    return (
+        <div className="EditToast">
+            <h1>Edit Toast</h1>
+        </div>
+    );
+  }
+}
+
+export default EditToast;
---
+++ src/components/pages/SingleBirthday/SingleBirthday.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './SingleBirthday.scss';
+
+class SingleBirthday extends React.Component {
+  render() {
+    return (
+        <div className="SingleBirthday">
+            <h1>Single Birthday</h1>
+        </div>
+    );
+  }
+}
+
+export default SingleBirthday;
