8b64320519bc748c2b06ccb5f9d0cdc028b5db6b
David Everett <davideverett1989@gmail.com>
2020-06-17 00:34:51 -0500

    Now reading user invited birthday events.

--- src/helpers/data/birthdayData.js
+++ src/helpers/data/birthdayData.js
@@ -3,6 +3,22 @@ import apiKeys from '../apiKeys.json';
 
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
+const getBirthdays = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/birthdays.json`)
+    .then((response) => {
+      const birthdayObject = response.data;
+      const birthdays = [];
+      if (birthdayObject !== null) {
+        Object.keys(birthdayObject).forEach((birthdayId) => {
+          birthdayObject[birthdayId].id = birthdayId;
+          birthdays.push(birthdayObject[birthdayId]);
+        });
+      }
+      resolve(birthdays);
+    })
+    .catch((err) => reject(err));
+});
+
 const getBirthdaysByCreatorUid = (creatorUid) => new Promise((resolve, reject) => {
   axios.get(`${baseUrl}/birthdays.json?orderBy="creatorUid"&equalTo="${creatorUid}"`)
     .then((response) => {
@@ -19,4 +35,6 @@ const getBirthdaysByCreatorUid = (creatorUid) => new Promise((resolve, reject) =
     .catch((err) => reject(err));
 });
 
-export default { getBirthdaysByCreatorUid };
+const getBirthdayById = (birthdayId) => axios.get(`${baseUrl}/birthdays/${birthdayId}.json`);
+
+export default { getBirthdaysByCreatorUid, getBirthdayById, getBirthdays };
