93bc974e25fc3fc6f26cb4b670a6922e8c8abdea
David Everett <davideverett1989@gmail.com>
2020-07-08 12:22:33 -0500

    Dashboard is responsive.

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -11,9 +11,9 @@ class Home extends React.Component {
         <div className="p-3 home-flex my-5 d-flex flex-column justify-content-center align-items-center">
           <h1 className="home-header display-1">Welcome To Apparty!</h1>
           <div className="my-5 big-image-container container-fluid">
-            <i className="col-12 col-sm-3 col-md-3 col-lg-3 col-xl-2 cake-image fas fa-birthday-cake fa-5x"></i>
+            <i className="col-12 col-sm-3 col-md-3 col-lg-3 col-xl-2 cake-image fas fa-birthday-cake fa-7x"></i>
             <i className="my-5 col-12 col-sm-3 col-md-3 col-lg-3 col-xl-2 people-image fas fa-people-arrows fa-7x"></i>
-            <i className="col-12 col-sm-3 col-md-3 col-lg-3 col-xl-2 cake-image fas img-fluid fa-birthday-cake fa-5x"></i>
+            <i className="col-12 col-sm-3 col-md-3 col-lg-3 col-xl-2 cake-image fas img-fluid fa-birthday-cake fa-7x"></i>
           </div>
           <p className="home-intro col-12 my-3 mx-auto">
             A socially distant birthday tracking and celebration app!
